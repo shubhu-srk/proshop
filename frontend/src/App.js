3e707c0eb86359a77d6c4e756be43286d0a7af59
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-21 11:14:33 +0530

    Header and Footer Components and React-Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homescreen from './screens/Homescreen';//Replaced with homescreen still pretty much same but added Outlet
+import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
-    <div>
-        <h1>Welcome to proshop</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
---
+++ frontend/src/screens/Homescreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Row , Col} from 'react-bootstrap';
+import products from '../products'
+import Product from '../components/Product';
+
+
+const Homescreen = () => {
+  return (
+    <>
+        <h1>Latest Products</h1>
+        <Row>
+            { products.map((product) => (
+                <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
+                </Col>
+            )) }
+        </Row>
+
+    </>
+  )
+}
+
+export default Homescreen
