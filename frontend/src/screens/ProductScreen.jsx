ceb70f9ea00e514a04048ce631504b199daf37ed
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-21 23:58:59 +0530

    PR-16: Fetching from Backend for Product Screen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import products from "../products";
+
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { useState, useEffect } from 'react';
+
+//Axios to fetch from Backend
+import axios from "axios";
+
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
-  console.log(product);
+  
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
   return (
     <>
       <Link to="/" className="btn btn-light my-3">
