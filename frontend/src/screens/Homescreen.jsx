f14eb2bb467f71cb157863dc0118b86e7604f937
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-21 23:43:33 +0530

    PR-16: Fetching from Backend for Homescreen

--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+
 import {Row , Col} from 'react-bootstrap';
 import products from '../products'
 import Product from '../components/Product';
+import { useSearchParams } from 'react-router-dom';
+
+import {useEffect, useState} from 'react'
+import axios from 'axios';
 
 
 const Homescreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
         <h1>Latest Products</h1>
