3a07a43b717c02efab26df25916f3f5e7b9341a4
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-22 16:39:38 +0530

    PR-28: Redux Store and API Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
+} from "react-router-dom";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import Homescreen from './screens/Homescreen';
-import ProductScreen from './screens/ProductScreen';
+import Homescreen from "./screens/Homescreen";
+import ProductScreen from "./screens/ProductScreen";
+
+import store from "./store";
+import { Provider } from "react-redux";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      <Route index={true} path='/' element={<Homescreen />} />
-      <Route path='/product/:id' element={<ProductScreen />} />
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Homescreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
-
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
 
 reportWebVitals();
---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+//PR-28: Redux Store and API Slice
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import { apiSlice } from './slices/apiSlice';
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+export default store;
