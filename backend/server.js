971c7d1f667d90a38d2dff63968999ae3a50b135
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-22 06:38:37 +0530

    PR-24: Get Products from database

--- backend/server.js
+++ backend/server.js
@@ -4,28 +4,18 @@ dotenv.config();
 import connectDB from './config/db.js';
 import products from './data/products.js'
 const port = process.env.PORT || 5000;
-
+import productRoutes from './routes/productRoutes.js';
 
 connectDB(); // Connect to MongoDB
 
 
 const app= express();
 
+app.use('/api/products', productRoutes);
 
 app.get('/',(req,res) => {
     res.send('API IS RUNNIG');
 })
-
-app.get('/api/products',(req,res)=>{
-    res.json(products);
-})
-
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product);
-})
-
-
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
