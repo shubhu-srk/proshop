35e941af4c16bc3c9927afac0e72b16048cc2dd3
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-24 02:09:45 +0530

    PR-56: Order routes and Controllers tested with Postman too

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import cookieParser from 'cookie-parser';
+import orderRoutes from './routes/orderRoutes.js';
 
 //Custom Error Middle Ware (PR-26)
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -24,6 +25,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 
 app.get('/',(req,res) => {
