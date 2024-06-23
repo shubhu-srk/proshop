2d3edbf23d5ecf543d959094f5c29ba3d9e443d3
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-23 07:12:03 +0530

    User Routes and Controllers

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ import connectDB from './config/db.js';
 import products from './data/products.js'
 const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 //Custom Error Middle Ware (PR-26)
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 
 connectDB(); // Connect to MongoDB
 
@@ -15,6 +17,8 @@ connectDB(); // Connect to MongoDB
 const app= express();
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 
 app.get('/',(req,res) => {
     res.send('API IS RUNNIG');
