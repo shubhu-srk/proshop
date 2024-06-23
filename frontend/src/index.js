a4d93702ee287624dacb2d0963dba3d8be5597e3
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-24 01:30:54 +0530

    PR-55: Payment method and its Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,7 +19,7 @@ import CartScreen from "./screens/CartScreen"
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen'
-
+import PaymentScreen from "./screens/PaymentScreen";
 import store from "./store";
 import { Provider } from "react-redux";
 import PrivateRoute from './components/PrivateRoute';
@@ -34,6 +34,7 @@ const router = createBrowserRouter(
       {/* Registered users */}
       <Route path='' element={<PrivateRoute />}>
         <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
       </Route>
     </Route>
   )
