cd9ee8971957b3562c24140983778f72155c6fa4
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-24 15:53:02 +0530

    PR-63: PayPal Buttons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,6 +25,7 @@ import { Provider } from "react-redux";
 import PrivateRoute from './components/PrivateRoute';
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from './screens/OrderScreen';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
@@ -49,7 +50,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+    <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
