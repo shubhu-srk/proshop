f92b113c6cb5c7ac8cc4d79914a477cbe05e969c
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-25 02:51:30 +0530

    PR-69: List Products for Admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,7 +28,7 @@ import OrderScreen from "./screens/OrderScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import AdminRoute from "./components/AdminRoute";
 import OrderListScreen from './screens/admin/OrderListScreen';
-
+import ProductListScreen from "./screens/admin/ProductListScreen";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const router = createBrowserRouter(
@@ -49,6 +49,7 @@ const router = createBrowserRouter(
       </Route>
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
+        <Route path="/admin/productlist" element={<ProductListScreen/>}/>
       </Route>
       
     </Route>
