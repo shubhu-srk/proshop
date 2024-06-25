87ece08e0a974e3c341409fc7ee72ecd617e9c7e
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-25 15:18:49 +0530

    PR-80: Create Reviews- Backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,11 @@ import express from "express";
 const router = express.Router();
 import {
   getProducts,
-  getProductById,createProduct,updateProduct,deleteProduct
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview
 } from '../controllers/productController.js';
 // import asyncHandler from "../middleware/asyncHandler.js";
 // import Product from "../models/productModel.js";
@@ -30,5 +34,10 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 // );
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router
+.route('/:id')
+.get(getProductById)
+.put(protect, admin, updateProduct)
+.delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 export default router;
